df8483e1e77929562159ea8c942e1f92fc1f5a39
yukas <67857615+Yukasik@users.noreply.github.com>
2024-08-01 15:01:10 +0500

    Add files via upload

---
+++ Tasks/Integer.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+// #include <math.h>
+
+void main() 
+{
+    // integer 1
+    // int L = 0;
+    // printf("Enter variable L ");
+    // scanf("%i", &L);
+    // printf("M = %i", L / 100);
+
+    // integer 2
+    // int M = 0;
+    // printf("Enter variable M ");
+    // scanf("%i", &M);
+    // printf("T = %i", M / 1000);
+
+    // integer 3
+    // int Byte = 0;
+    // printf("Enter variable Byte ");
+    // scanf("%i", &Byte);
+    // printf("Kilobyte = %i", Byte / 1024);
+
+    // integer 4
+    // int A = 0, B = 0;
+    // printf("Enter variables A and B ");
+    // scanf("%i %i", &A, &B);
+    // printf("B = %i", A / B);
+
+    // integer 5
+    // int A = 0, B = 0;
+    // printf("Enter variables A and B ");
+    // scanf("%i %i", &A, &B);
+    // printf("A = %i", A % B);
+
+    // integer 6
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);
+    // printf("l = %i\nr = %i", a / 10, a % 10);
+
+    // integer 7
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);    
+    // printf("Sum = %i\nP = %i", (a / 10) + (a % 10), (a / 10) * (a % 10));
+
+    // integer 8
+    // int a = 0;
+    // printf("Enter variable a "); 
+    // scanf("%i", &a);
+    // printf("b = %i", (a % 10 * 10) + (a / 10));
+
+    // integer 9
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);
+    // printf("s = %i", a / 100);
+
+    // integer 10
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);
+    // printf("b = %i\nc = %i", a % 10, (a / 10) % 10);
+
+    // integer 11 
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);
+    // printf("Sum = %i\nP = %i", (a / 100) + ((a / 10) % 10) + (a % 10), (a / 100) * ((a / 10) % 10) * (a % 10));
+
+    // integer 12
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);
+    // printf("b = %i", (a % 10 * 100) + (((a / 10) % 10) * 10) + (a / 100));
+
+    // integer 13
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);
+    // printf("b = %i", (((a / 10) % 10) * 100) + ((a % 10) * 10) + (a / 100));
+
+    // integer 14
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);
+    // printf("b = %i", ((a % 10) * 100) + (a / 10));
+
+    // integer 15
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);
+    // printf("b = %i", ((a / 10)  % 10 * 100) + (a / 100 * 10) + (a % 10));
+
+    // integer 16
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);
+    // printf("b = %i", (a / 100 * 100) + (a % 10 * 10) + ((a / 10) % 10));
+
+    // integer 17
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);
+    // printf("b = %i", (a / 100) % 10);
+
+    // integer 18
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);
+    // printf("b = %i", (a / 1000) % 10);
+
+    // integer 19
+    // int N = 0;
+    // printf("Enter variable N ");
+    // scanf("%i", &N);
+    // printf("Min = %i", N / 60);
+
+    // integer 20
+    // int N = 0;
+    // printf("Enter variable N ");
+    // scanf("%i", &N);
+    // printf("Hour = %i", N / 3600);
+
+    // integer 21
+    // int N = 0;
+    // printf("Enter variable N ");
+    // scanf("%i", &N);
+    // printf("Seconds = %i", N % 60);
+
+    // integer 22
+    // int N = 0;
+    // printf("Enter variable N ");
+    // scanf("%i", &N);
+    // printf("Seconds = %i", N % 3600);
+
+    // integer 23
+    // int N = 0;
+    // printf("Enter variable N ");
+    // scanf("%i", &N);
+    // printf("Min = %i", (N / 60) % 3600);
+
+    // integer 24
+    // int K = 0;
+    // printf("Enter variable K ");
+    // scanf("%i", &K);
+    // printf("Week number = %i", K % 7);
+
+    // integer 25
+    // int K = 0;
+    // printf("Enter variable K ");
+    // scanf("%i", &K);
+    // printf("Week number = %i", (K + 3) % 7);
+
+    // integer 26
+    // int K = 0;
+    // printf("Enter variable K ");
+    // scanf("%i", &K);
+    // printf("Week number = %i", (K + 1) % 7);
+
+    // integer 27
+    // int K = 0;
+    // printf("Enter variable K ");
+    // scanf("%i", &K);
+    // printf("Week number = %i", (K + 5) % 7);
+
+    // integer 28
+    // int K = 0, N = 0;
+    // printf("Enter variables K and N ");
+    // scanf("%i %i", &K, &N);
+    // printf("Week number %i", (K - 1 + N - 1) % 7) + 1;
+
+    // integer 29
+    // int A = 0, B = 0, C = 0;
+    // printf("Enter variables A, B and C ");
+    // scanf("%i %i %i", &A, &B, &C);
+    // printf("K = %i\nS = %i", (A / C) * (B / C), A * B - ((A / C) * (B / C) * (C * C)));
+
+    // integer 30
+    // int year = 0;
+    // printf("Enter variable year ");
+    // scanf("%i", &year);
+    // printf("Century %i", (year - 1) / 100 + 1);
+}
