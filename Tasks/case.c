819b0df200f72c9c9814e24942a83fe68663b934
yukas <67857615+Yukasik@users.noreply.github.com>
2024-09-27 19:10:00 +0500

    Add files via upload

---
+++ Tasks/case.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void main() 
+{
+    // case 1
+    // int a = 0;
+    // printf("Enter variable a ");
+    // scanf("%i", &a);
+    // switch(a) {
+    // case 1: printf("Monday"); break;
+    // case 2: printf("Tuesday"); break;
+    // case 3: printf("Wednesday"); break;
+    // case 4: printf("Thurday"); break;
+    // case 5: printf("Friday"); break;
+    // case 6: printf("Saturday"); break;
+    // case 7: printf("Sunday"); break;
+    // default: printf("ERROR"); break;
+    // }
+
+    // enum WeekDays { Monday = 1, Tuesday, Wednesday, Thurday, Friday, Saturday, Sunday};
+    // switch(a) {
+    //     case Monday: printf("Monday"); break;
+    //     case Tuesday: printf("Tuesday"); break;
+    //     case Wednesday: printf("Wednesday"); break;
+    //     case Thurday: printf("Thurday"); break;
+    //     case Friday: printf("Friday"); break;
+    //     case Saturday: printf("Saturday"); break;
+    //     case Sunday: printf("Sunday"); break;
+    // }
+
+    // case 2
+    // int K = 0;
+    // printf("Enter variable K ");
+    // scanf("%i", &K);
+    // switch(K) {
+    //     case 1: printf("Bad"); break;
+    //     case 2: printf("Unbad"); break;
+    //     case 3: printf("Good"); break;
+    //     case 4: printf("Nice"); break;
+    //     case 5: printf("Great"); break;
+    //     default: printf("ERROR"); break;
+    // }
+
+    // enum Assessments { Bad = 1, Unbad, Good, Nice, Great };
+    // switch(K) {
+    //     case Bad: printf("Bad"); break;
+    //     case Unbad: printf("Unbad"); break;
+    //     case Good: printf("Good"); break;
+    //     case Nice: printf("Nice"); break;
+    //     case Great: printf("Great"); break;
+    //     default: printf("ERROR"); break;
+    // }
+}
